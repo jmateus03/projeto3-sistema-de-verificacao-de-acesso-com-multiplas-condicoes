fb8d80cfafdd7ef9c59ade00973342dbae19cf47
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-09-09 11:06:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ if (idade >= 18){
    alert("Bem-vindo, Admin! Acesso total liberado.")
 }else if (nome === "moderador" && idade >= 16) {
    alert("Olá, Moderador! Acesso liberado para a área de moderação.")
-}else if (idade >= 13 || idade <= 17){
+}else if (idade >= 13 && idade <= 17){
    alert("negado. Você pode participar do nosso programa Jovem Aprendiz!")
 }else{
    alert("Acesso negado. O conteúdo é exclusivo para maiores de 18 anos.")
+
 }
